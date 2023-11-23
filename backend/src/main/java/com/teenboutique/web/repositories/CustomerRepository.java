7ee4aed02c5319f51fbfc5d437f87526d42fe3f1
thanh-bb <carrot1420@gmail.com>
2023-11-23 22:34:15 +0700

    repush

--- backend/src/main/java/com/teenboutique/web/repositories/CustomerRepository.java
+++ backend/src/main/java/com/teenboutique/web/repositories/CustomerRepository.java
@@ -2,7 +2,6 @@ package com.teenboutique.web.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.teenboutique.web.entities.Category;
 import com.teenboutique.web.entities.Customer;
 
 public interface CustomerRepository  extends JpaRepository<Customer, Long>{
