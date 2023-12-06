c29a78373395164fcc6e8bd63ed888015b44bf70
AnthonyPA0902 <ioephuocan@gmail.com>
2023-12-06 15:25:36 +0700

    Cập nhật phần xác thực Front End

---
+++ frontend/src/pages/Forget.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+const Forget = () => {
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [formData, setFormData] = useState({
+        email: '',
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/api/forget', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                setSuccessMessage('Một Mail Chứa Mật Khẩu Mới Của Bạn Đã Được Gửi');
+                setErrorMessage('');
+                console.log('Send email succesful')
+            } else {
+                setSuccessMessage('');
+                setErrorMessage('Email Không Tồn Tại Với Tài Khoản Nào');
+                console.log('Email is incorrect')
+            }
+        } catch (error) {
+            console.error('Error sending email:', error);
+            setSuccessMessage('');
+            setErrorMessage('Không thể gửi email. Vui lòng thử lại');
+        }
+    };
+
+    return (
+        <section className="forget-password-page account">
+        <div className="container">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+                <div className="block text-center">
+                <a className="logo" href="/">
+                    <img src="/assets/images/logo.png" alt="Logo" />
+                </a>
+                <h2 className="text-center">Chào Mừng Trở Lại</h2>
+                <form className="text-left clearfix" onSubmit={handleSubmit}>
+                    <p>
+                    Hãy nhập vào địa chỉ email của tài khoản bạn. Một mã số xác thực sẽ được gửi đến bạn. Một khi bạn nhận được mã số xác thực, bạn sẽ có thể tạo một mật khẩu mới cho tài khoản của bạn.
+                    </p>
+                    <div className="form-group">
+                    <input type="email" className="form-control" name="email" placeholder="Email Của Tài Khoản" value={formData.email} onChange={handleChange}/>
+                    </div>
+                    <div className="text-center">
+                    <button type="submit" className="btn btn-main text-center">Đặt Lại Mật Khẩu</button>
+                    </div>
+                    {successMessage && (
+                        <div className="alert alert-success" role="alert">
+                            <p>{successMessage}</p>
+                        </div>
+                    )}
+                    {errorMessage && (
+                        <div className="alert alert-danger" role="alert">
+                            <p>{errorMessage}</p>
+                        </div>
+                    )}
+                </form>
+                <p className="mt-20"><a href="/login">Quay Trở Về Đăng Nhập</a></p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+    );
+};
+
+export default Forget;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+    const [error, setError] = useState(false);
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: '',
+        dob: '',
+        address: '',
+        phone: '',
+        gender: true,
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        setFormData({ 
+        ...formData, 
+        [e.target.name]: e.target.value 
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            if (response.ok) {
+                navigate('/login');
+                console.log('Register successful');
+            } else {
+                setError(true);
+                console.error('Register failed');
+            } 
+        } catch (error) {
+            setError(true);
+            navigate(-1);
+            console.error('Error:', error);
+        }
+
+        const newErrors = {};
+
+        if (!formData.name) {
+            newErrors.name = 'Tên không được để trống';
+        }
+
+        const currentDate = new Date();
+        const Dob = new Date(formData.dob)
+        if (Dob >= currentDate) {
+            newErrors.dob = 'Ngày Sinh Phải Ở Quá Khứ';
+        }
+
+        if (!formData.address) {
+            newErrors.address = 'Địa chỉ không được để trống';
+        }
+
+        if (!/^[0-9]{10}$/.test(formData.phone)) {
+            newErrors.phone = 'Số điện thoại phải có 10 số';
+        }
+
+        if (!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/.test(formData.email)) {
+            newErrors.email = 'Email không hợp lệ';
+        } else if (formData.email.length < 10 || formData.email.length > 50) {
+            newErrors.email = 'Email phải từ 10 đến 50 ký tự';
+        }
+
+        if (!formData.password) {
+            newErrors.password = 'Mật khẩu không được để trống';
+        }
+
+        setError(newErrors);
+
+        if (Object.keys(newErrors).length === 0) {
+            console.log('Form submitted:', formData);
+        }
+    }
+
+    return (
+      <section className="signin-page account">
+        <div className="container">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+                <div className="block text-center">
+                <a className="logo" href="/">
+                    <img src="/assets/images/logo.png" alt="Logo" />
+                </a>
+                <h2 className="text-center">Tạo Tài Khoản Mới</h2>
+                <form className="text-left clearfix" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                    <input type="text" className="form-control" name="name" placeholder="Tên" value={formData.name} onChange={handleChange}/>
+                    {error.name && <div className="text-danger">{error.name}</div>}
+                    </div>
+                    <div className="form-group">
+                    <input type="date" className="form-control" name="dob" placeholder="Ngày Sinh" value={formData.dob} onChange={handleChange}/>
+                    {error.dob && <div className="text-danger">{error.dob}</div>}
+                    </div>
+                    <div className="form-group">
+                    <input type="text" className="form-control" name="address" placeholder="Địa Chỉ" value={formData.address} onChange={handleChange}/>
+                    {error.address && <div className="text-danger">{error.address}</div>}
+                    </div>
+                    <div className="form-group">
+                    <input type="text" className="form-control" name="phone" placeholder="Số Điện Thoại" value={formData.phone} onChange={handleChange}/>
+                    {error.phone && <div className="text-danger">{error.phone}</div>}
+                    </div>
+                    <div className="form-group">
+                    <label>Giới tính</label>
+                    <select className="form-control btn btn-outline-primary dropdown-toggle" name="gender" value={formData.gender} onChange={handleChange}>
+                        <option value="true">Nam</option>
+                        <option value="false">Nữ</option>
+                    </select>
+                    </div>
+                    <div className="form-group">
+                    <input type="email" className="form-control" name="email" placeholder="Email" value={formData.email} onChange={handleChange}/>
+                    {error.email && <div className="text-danger">{error.email}</div>}
+                    </div>
+                    <div className="form-group">
+                    <input type="password" className="form-control" name="password" placeholder="Mật Khẩu" value={formData.password} onChange={handleChange}/>
+                    </div>
+                    {error.password && <div className="text-danger">{error.password}</div>}
+                    <div className="text-center">
+                    <button type="submit" className="btn btn-main text-center">Đăng Ký</button>
+                    </div>
+                </form>
+                <p className="mt-20">Đã có tài khoản ?<a href="/login"> Đăng Nhập </a></p>
+                <p><a href="/forget"> Quên Mật Khẩu?</a></p>
+                </div>
+            </div>
+            </div>
+        </div>
+      </section>
+    );
+};
+
+export default Register;
