cd9038aaa5ee96306eae2f826e22a816b5200f99
hoangngan662 <nganb2005765@student.ctu.edu.vn>
2023-12-10 21:31:19 +0700

    sua product repo

--- backend/src/main/java/com/teenboutique/web/repositories/ProductRepository.java
+++ backend/src/main/java/com/teenboutique/web/repositories/ProductRepository.java
@@ -18,7 +18,7 @@ public interface ProductRepository  extends JpaRepository<Product, Long> {
 	 @Query(value = "SELECT product.* FROM product inner join (select product_id from customer_order_item group by product_id order by sum(quantity) desc limit 9) my on product.id = my.product_id;", nativeQuery=true)
 	List<Product> findTrendyProduct();
 
-	 @Query(value = "SELECT * FROM groupdb.product WHERE stop_sale = false AND category_id = ?1", nativeQuery=true)
+	 @Query(value = "SELECT * FROM product WHERE stop_sale = false AND category_id = ?1", nativeQuery=true)
 	Page<Product> findAllProductCate(Long id, Pageable page);
 
 }
